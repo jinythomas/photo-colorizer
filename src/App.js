a5f39ab3267f29085e1b14c9699912870de66c03
rony thankachan <rony.mail2me@gmail.com>
2022-09-09 03:05:18 +0530

    frontend basics completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [originalImg, setOriginalImg] = useState(null);
+  const [coloredImg, setColoredImg] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const uploadImage = (e) => {
+    setOriginalImg(URL.createObjectURL(e.target.files[0]));
+    setColoredImg(null);
+  };
+
+  const convertImg = (e) => {
+    e.preventDefault();
+    setColoredImg(null);
+    setLoading(true);
+    // upload file
+    const formData = new FormData();
+    formData.append("File", originalImg);
+    fetch("https://localhost:8000/colorize", { method: "POST", body: formData })
+      .then((result) => {
+        setColoredImg(originalImg);
+        setLoading(false);
+      })
+      .catch((err) => {
+        alert(err);
+        setLoading(false);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Photo colorizer</h1>
+      <div className="container">
+        {/* Original image container */}
+        <div className="original">
+          {originalImg ? (
+            <img src={originalImg} alt="Uncolored" />
+          ) : (
+            "No image found"
+          )}
+        </div>
+
+        {/* Control buttons */}
+        <div className="controls">
+          <input type="file" accept="image/*" onChange={uploadImage} />
+          {originalImg && (
+            <button className="convertBtn" onClick={convertImg}>
+              {loading ? "Converting.. " : "Convert"}
+            </button>
+          )}
+        </div>
+
+        {/* Colored image container */}
+        <div className="colored">
+          {coloredImg ? (
+            <img src={coloredImg} alt="Colored" />
+          ) : (
+            "No image found"
+          )}
+        </div>
+      </div>
     </div>
   );
 }
